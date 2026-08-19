9c1313ce0b19ace6c78f46839e3e838039607648
rafael casagrande <rafaehlcasagrande@gmail.com>
2026-08-19 18:44:35 -0300

    Mudando a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você não vai andar comigo Viviane"</h1>
+    return <h1>Feliz Aniversário atrasado José ! A gorda ta indo andar essa hora !!</h1>
 }
 
 export default Home
