a249b6517243c0d7f7b8d07c2c9e4795a68aeeee
rafael casagrande <rafaehlcasagrande@gmail.com>
2026-09-22 01:49:17 -0300

    Primeiros ajustes em `/status` e testes de validacao

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,17 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const databaseConsultVersion = await database.query("SHOW server_version");
+  const databaseMaxConnections = await database.query("SHOW max_connections");
+  const databaseActivityConnections = await database.query(
+    "SELECT count(1),state FROM pg_stat_activity GROUP BY state",
+  );
   response.status(200).json({
-    chave: "São ótimos",
+    updated_at: updatedAt,
+    database_version: databaseConsultVersion.rows[0].server_version,
+    database_max_connections: databaseMaxConnections.rows[0].max_connections,
+    database_connections: databaseActivityConnections.rows[1].count,
   });
 }
 
