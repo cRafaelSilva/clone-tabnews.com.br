1ed61e2dae484a22adfed53ce25a1c7f08b815e3
rafael casagrande <rafaehlcasagrande@gmail.com>
2026-09-22 23:48:44 -0300

    refactor of `database.js`file

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,14 @@
 import { Pool } from "pg";
 
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
 async function query(queryObj) {
-  const pool = new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    masLifetimeSeconds: 60,
-  });
-
-  //await pool.connect();
   const result = await pool.query(queryObj);
-  //await pool.end();
   return result;
 }
 
