c5809f7bcc4abf3afc20f839f89fff2113b849f9
rafael casagrande <rafaehlcasagrande@gmail.com>
2026-09-25 22:10:49 -0300

    refactor tests in `get.test.js` to consolidate database status checks

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,44 +1,10 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
   expect(response.status).toBe(200);
-});
-
-test("GET database last update", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
   const responseBody = await response.json();
-
-  expect(responseBody.updated_at).toBeDefined();
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-});
-
-test("GET database version", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-
-  expect(responseBody.database_version).toBeDefined();
-  const databaseVersion = Number(responseBody.database_version);
-  expect(databaseVersion).not.toBeNaN();
-});
-
-test("GET database max connections", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-
-  expect(responseBody.database_max_connections).toBeDefined();
-  expect(Number(responseBody.database_max_connections)).not.toBeNaN();
-});
-
-test("Get database activity connections", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-
-  expect(responseBody.database_connections).toBeDefined();
-
-  const databaseActivityConnections = Number(responseBody.database_connections);
-  expect(databaseActivityConnections).not.toBeNaN();
-  expect(String(databaseActivityConnections)).toEqual(
-    responseBody.database_connections,
-  );
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
